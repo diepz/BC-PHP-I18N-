eab69662ce45c35115b34ef5a0f8868ebbe003ff
diepz <you@example.cm>
2018-11-30 14:02:25 +0700

    done

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: diepzdeptraivodich
+ * Date: 30/11/2018
+ * Time: 13:57
+ */
+return [
+    'title' => 'Message was written by english.',
+    'content' => 'FUCK YOU'
+];
---
+++ resources/lang/vi/messages.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: diepzdeptraivodich
+ * Date: 30/11/2018
+ * Time: 13:58
+ */
+return [
+    'title' => 'Tin nhắn được viết bằng tiếng việt.',
+    'content' => 'AHIHI ĐỒ NGỐC'
+];
